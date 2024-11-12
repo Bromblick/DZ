a2f55a5971a0884b608dd4c3142921e78bb6fd92
Bromblick <mr.booblick@gmail.com>
2024-11-12 20:05:35 +0500

    Add files via upload

---
+++ Базовые структуры данных.py
@@ -0,0 +1,21 @@
+# "1st program"
+print(9**0.5*5)
+
+print(u'\u2500' * 10)
+
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+print(u'\u2500' * 10)
+
+# "3rd program"
+a=(2*2+2)
+print(a)
+b=(2*(2+2))
+print(b)
+print(a==b)
+
+print(u'\u2500' * 10)
+
+#"4th program"
+print(int(float('123.456')*10)%10)
